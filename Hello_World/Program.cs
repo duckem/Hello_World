96c335c9f46c0931418b08ae8995ce82f48d293f
Mike Duckert <Mike Duckert>
2017-07-03 10:42:24 -0500

    Add project files.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main()
+        {
+            EnterName();
+            Loop(5);
+        }
+
+        static void EnterName()
+        {
+            Console.WriteLine("Please enter your name");
+
+            try
+            {
+                string nameEntry = Console.ReadLine();
+                if (nameEntry.Length > 0)
+                {
+                    //Console.WriteLine($"Welcome {nameEntry}!");
+                    Console.WriteLine(UserName(nameEntry));
+                    Console.Read();
+                }
+                else
+                {
+                    Console.WriteLine("Please try again.");
+                    Main();
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("Throw error here.");
+            }
+        }
+
+        static string UserName (string name)
+         {
+            return $"Welcome {name}. Press 'h' for Help";
+         }
+
+        static void Loop (int looping)
+        {
+            for (looping = 1; looping < 11; looping++)
+            {
+                Console.WriteLine(looping);
+            }
+        }
+    }
+}
